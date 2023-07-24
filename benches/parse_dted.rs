60c5d05612110b09e6e987e891ecc615bc6cb4b7
Jakob Widauer <jakob.widauer@gmail.com>
2023-07-25 01:44:15 +0200

    feat: add tile parsing benchmark

---
+++ benches/parse_dted.rs
@@ -0,0 +1,20 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let resource_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("resources");
+    let path = resource_dir.join("n47.dt2");
+    let file = File::open(path).unwrap();
+    let mut reader = BufReader::new(file);
+    let mut buffer = Vec::new();
+    reader.read_to_end(&mut buffer).unwrap();
+
+    c.bench_function("parse_tile", |b| {
+        b.iter(|| spdted::DtedTile::from_bytes(&buffer).unwrap())
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
